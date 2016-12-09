a64694897d9c4a9d8cbc97f74a9dc79b09786b42
iliyan-kostov <iliyan.kostov.gml@gmail.com>
2016-12-09 13:29:22 +0200

    Added private field "clientPrimaryKeyValue" and respective accessor and mutator to class "networking.Message".

--- src/networking/Message.java
+++ src/networking/Message.java
@@ -11,19 +11,34 @@ import java.io.Serializable;
  * {@link Message#type} is implemented as {@link String} so that the class is
  * extensible and forward-compatible. It is expected that within the same system
  * all different derived classes use different values for the
- * {@link Message#type} field.
+ * {@link Message#type} field. The field {@link Message#clientPrimaryKeyValue}
+ * can be used for setting or accessing either the sender or the receiver for
+ * the message. The client ID for a specific {@link Message} is intended to be
+ * set and controlled by the server through authentication procedures once the
+ * message enters the system's boundary.
  *
  * @author iliyan-kostov <iliyan.kostov.gml@gmail.com>
  */
 public abstract class Message implements Serializable {
 
+    private String clientPrimaryKeyValue;
+
     private final String type;
 
     public Message(String type) {
         this.type = type;
+        this.clientPrimaryKeyValue = null;
     }
 
     public final String getType() {
         return this.type;
+    }
+
+    public final String getClientPrimaryKeyValue() {
+        return this.clientPrimaryKeyValue;
+    }
+
+    public final void setClientPrimaryKeyValue(String clientPrimaryKeyValue) {
+        this.clientPrimaryKeyValue = clientPrimaryKeyValue;
     }
 }
