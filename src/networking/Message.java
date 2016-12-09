a4bb34778d2c03b2f7c26280d51968e1bf1d0d9c
iliyan-kostov <iliyan.kostov.gml@gmail.com>
2016-12-09 08:25:25 +0200

    Changed "networking.Message.type" and all related methods to "String" type. Changed "Message" class to abstract.

--- src/networking/Message.java
+++ src/networking/Message.java
@@ -3,23 +3,27 @@ package networking;
 import java.io.Serializable;
 
 /**
+ * {@link Message} is a {@link Serializable} class that serves the transmission
+ * of data over the network. {@link Message} is an abstract base class that
+ * holds a private field {@link Message#type}. This field is used to recognize
+ * the specific derived class in order to be able to successfully typecast the
+ * base serialized {@link Message} reference to its actual intended type.
+ * {@link Message#type} is implemented as {@link String} so that the class is
+ * extensible and forward-compatible. It is expected that within the same system
+ * all different derived classes use different values for the
+ * {@link Message#type} field.
  *
  * @author iliyan-kostov <iliyan.kostov.gml@gmail.com>
  */
-public class Message implements Serializable {
+public abstract class Message implements Serializable {
 
-    public enum TYPE {
-        ANY
-        // TBA
-    };
+    private final String type;
 
-    private final TYPE type;
-
-    public Message(TYPE type) {
+    public Message(String type) {
         this.type = type;
     }
 
-    public final TYPE getType() {
+    public final String getType() {
         return this.type;
     }
 }
