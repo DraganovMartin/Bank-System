cd73f8aeeecc58f9fea6b1c921c74e796a981ee0
iliyan-kostov <iliyan.kostov.gml@gmail.com>
2016-12-11 09:47:27 +0200

    Added money conversion method and updated test class.

--- src/dataModel/CurrencyExchangeInfo.java
+++ src/dataModel/CurrencyExchangeInfo.java
@@ -19,6 +19,13 @@ import java.util.TreeMap;
  * precision to be used when performing conversions, especially divisions, as
  * defined by
  * {@link BigDecimal#divide(java.math.BigDecimal, int, java.math.RoundingMode)}.
+ * <p>
+ * The class is intended to provide synchronized operations so its instances can
+ * be accessed from many concurrent threads while preserving data integrity (for
+ * example when modifying current exchange rates). The synchronization should be
+ * performed to the data source ({@link CurrencyExchangeInfo#currencyValues}) to
+ * ensure that all operations performed used a valid snapshot of the exchange
+ * rates. This policy should also be enforced in derived classes.
  *
  * @see
  * {@link BigDecimal#divide(java.math.BigDecimal, int, java.math.RoundingMode)}
@@ -37,6 +44,8 @@ public class CurrencyExchangeInfo implements Serializable {
      * non-positive relative values. The class implements {@link Serializable}
      * so a serialized version of the current exchange information can be sent
      * over the network.
+     * <p>
+     * Operations that access or modify this object should synchronize to it.
      */
     private final TreeMap<Currency, BigDecimal> currencyValues;
 
@@ -102,9 +111,11 @@ public class CurrencyExchangeInfo implements Serializable {
      *
      * @return true if successful, otherwise false.
      */
-    synchronized public final boolean setCurrencyValue(Currency currency, BigDecimal value) {
+    public final boolean setCurrencyValue(Currency currency, BigDecimal value) {
         if (value.compareTo(BigDecimal.ZERO) > 0) {
-            this.currencyValues.put(currency, value);
+            synchronized (this.currencyValues) {
+                this.currencyValues.put(currency, value);
+            }
             return true;
         } else {
             return false;
@@ -122,17 +133,18 @@ public class CurrencyExchangeInfo implements Serializable {
      *
      * @param valueArr an array of the relative values to set.
      */
-    synchronized public final void setCurrencyValuesMultiple(Currency[] currencyArr, BigDecimal[] valueArr) {
+    public final void setCurrencyValuesMultiple(Currency[] currencyArr, BigDecimal[] valueArr) {
         if (currencyArr.length == valueArr.length) {
             int l = currencyArr.length;
-            for (int i = 0; i < l; i++) {
-                if ((currencyArr[i] != null) && (valueArr[i] != null)) {
-                    if (valueArr[i].compareTo(BigDecimal.ZERO) > 0) {
-                        this.currencyValues.put(currencyArr[i], valueArr[i]);
+            synchronized (this.currencyValues) {
+                for (int i = 0; i < l; i++) {
+                    if ((currencyArr[i] != null) && (valueArr[i] != null)) {
+                        if (valueArr[i].compareTo(BigDecimal.ZERO) > 0) {
+                            this.currencyValues.put(currencyArr[i], valueArr[i]);
+                        }
                     }
                 }
             }
-
         }
     }
 
@@ -150,11 +162,13 @@ public class CurrencyExchangeInfo implements Serializable {
      *
      * @see {@link CurrencyExchangeInfo}
      */
-    synchronized public final BigDecimal getExchangeRate(Currency currencyFrom, Currency currencyTo) {
+    public final BigDecimal getExchangeRate(Currency currencyFrom, Currency currencyTo) {
         BigDecimal valueFrom;
         BigDecimal valueTo;
-        valueFrom = this.currencyValues.get(currencyFrom);
-        valueTo = this.currencyValues.get(currencyTo);
+        synchronized (this.currencyValues) {
+            valueFrom = this.currencyValues.get(currencyFrom);
+            valueTo = this.currencyValues.get(currencyTo);
+        }
         if ((valueFrom != null) && (valueTo != null)) {
             return valueTo.divide(valueFrom, this.scale, this.roundingMode);
         } else {
@@ -167,7 +181,9 @@ public class CurrencyExchangeInfo implements Serializable {
      * drawn from the current key-value mapping. It describes the equivalence of
      * a certain amount of a specified currency type when converted to any of
      * the supported types of currency. Uses the predefined {@link RoundingMode}
-     * and scale.
+     * and scale. Can be used to convert between currencies. Useful when
+     * converting to and from a currency with a low relative value (for example
+     * one measured in thousands of currency units).
      *
      * @param baseCurrency the type of currency to convert from. Can not be
      * NULL.
@@ -181,7 +197,7 @@ public class CurrencyExchangeInfo implements Serializable {
      *
      * @see {@link CurrencyExchangeInfo}
      */
-    synchronized public final TreeMap<Currency, BigDecimal> getCurrencyExchangedValues(Currency baseCurrency, BigDecimal baseAmount) {
+    public final TreeMap<Currency, BigDecimal> getCurrencyExchangeMap(Currency baseCurrency, BigDecimal baseAmount) {
         if ((baseCurrency != null) && (baseAmount != null)) {
             synchronized (this.currencyValues) {
                 BigDecimal baseValue = this.currencyValues.get(baseCurrency);
@@ -195,6 +211,83 @@ public class CurrencyExchangeInfo implements Serializable {
                     return null;
                 }
             }
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Returns a {@link CurrencyExchangeInfo} object with information drawn from
+     * the current key-value mapping. It describes the equivalence of a certain
+     * amount of a specified currency type when converted to any of the
+     * supported types of currency. Uses the predefined {@link RoundingMode} and
+     * scale. Can be used to convert between currencies. Useful when converting
+     * to and from a currency with a low relative value (for example one
+     * measured in thousands of currency units). Allows the change of the
+     * specified {@link RoundingMode} and scale.
+     *
+     * @param baseCurrency the type of currency to convert from. Can not be
+     * NULL.
+     *
+     * @param baseAmount the amount of money of the base currency type. Can not
+     * be NULL.
+     *
+     * @param roundingMode the {@link RoundingMode} to use in the new object.
+     *
+     * @param scale the scale to use in the new object.
+     *
+     * @return a {@link TreeMap<Currency, BigDecimal>} object generated based on
+     * the current state, NULL if failed (i.e. undefined currency type
+     * provided).
+     *
+     * @see {@link CurrencyExchangeInfo}
+     */
+    public final CurrencyExchangeInfo getCurrencyExchangeInfo(Currency baseCurrency, BigDecimal baseAmount, RoundingMode roundingMode, int scale) {
+        if ((baseCurrency != null) && (baseAmount != null)) {
+            synchronized (this.currencyValues) {
+                BigDecimal baseValue = this.currencyValues.get(baseCurrency);
+                if (baseValue != null) {
+                    CurrencyExchangeInfo result = new CurrencyExchangeInfo(roundingMode, scale);
+                    for (Map.Entry<Currency, BigDecimal> entry : this.currencyValues.entrySet()) {
+                        result.setCurrencyValue(entry.getKey(), entry.getValue().multiply(baseAmount).divide(baseValue, this.scale, this.roundingMode));
+                    }
+                    return result;
+                } else {
+                    return null;
+                }
+            }
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Returns a {@link Money} object of the requested currency with value equal
+     * to the value of the provided {@link Money} object, according to the
+     * current exchange rates.
+     *
+     * @param moneyFrom the {@link Money} provided.
+     *
+     * @param currencyTo the requested currency to convert to.
+     *
+     * @return a {@link Money} object of the requested currency with value equal
+     * to the value of the provided {@link Money} object, according to the
+     * current exchange rates. Returns NULL if the conversion fails (i.e.
+     * requested currency not supported).
+     */
+    public Money getConvertedMoney(Money moneyFrom, Currency currencyTo) {
+        BigDecimal amountFrom = moneyFrom.getAmount();
+        BigDecimal amountTo;
+        Currency currencyFrom = moneyFrom.getCurrency();
+        BigDecimal valueFrom;
+        BigDecimal valueTo;
+        synchronized (this.currencyValues) {
+            valueFrom = this.currencyValues.get(currencyFrom);
+            valueTo = this.currencyValues.get(currencyTo);
+        }
+        if ((valueFrom != null) && (valueTo != null)) {
+            amountTo = amountFrom.multiply(valueTo).divide(valueFrom, this.scale, this.roundingMode);
+            return Money.createMoney(currencyTo, amountTo);
         } else {
             return null;
         }
