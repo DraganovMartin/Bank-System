3b3faeaba744712b601746741815eddd252da2e4
iliyan-kostov <iliyan.kostov.gml@gmail.com>
2016-12-14 07:47:09 +0200

    Reworked "Money" class to only allow a fixed precision (two decimal digits fractional part) that allows operating with cents. Added a Money factory method that uses String input. Added a test class.

--- src/dataModel/Money.java
+++ src/dataModel/Money.java
@@ -1,16 +1,32 @@
 package dataModel;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * Represents a set amount of money of a specific currency. Provides a factory
- * that only allows creating objects with legal values (i.e. not NULL).
- *
- * @see {@link Currency}
+ * that only allows creating objects with legal values (i.e. not NULL). All
+ * {@link Money} objects:
+ * <p>
+ * - work with a fixed precision (two decimal digits fractional part) that
+ * allows operating with cents;
+ * <p>
+ * - use a fixed {@link RoundingMode} - {@link RoundingMode#HALF_UP}
  *
  * @author iliyan-kostov <iliyan.kostov.gml@gmail.com>
  */
 public class Money {
+
+    /**
+     * The {@link RoundingMode} to use for all {@link Money} objects.
+     */
+    private final static RoundingMode ROUNDINGMODE = RoundingMode.HALF_UP;
+
+    /**
+     * The precision (decimal digits fractional part) to use for all
+     * {@link Money} objects.
+     */
+    private final static int SCALE = 2;
 
     /**
      * The {@link Currency} of the money.
@@ -27,11 +43,12 @@ public class Money {
      *
      * @param currency the {@link Currency} of the money.
      *
-     * @param amount the amount of the money.
+     * @param amount the amount of the money. The amount is rounded according to
+     * the value of {@link Money#SCALE} and {@link Money#ROUNDINGMODE} fields.
      */
     private Money(Currency currency, BigDecimal amount) {
         this.currency = currency;
-        this.amount = amount;
+        this.amount = amount.divide(BigDecimal.ONE, SCALE, ROUNDINGMODE);
     }
 
     /**
@@ -48,6 +65,35 @@ public class Money {
     public static final Money createMoney(Currency currency, BigDecimal amount) {
         if ((currency != null) && (amount != null)) {
             return new Money(currency, amount);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Returns an object that represents a certain amount of money of the
+     * specified currency.
+     *
+     * @param currency the {@link Currency} of the money. Can not be NULL.
+     *
+     * @param amount the amount of the money. Can not be NULL.
+     *
+     * @return an object that represents a certain amount of money of the
+     * specified currency, NULL if failed.
+     */
+    public static final Money createMoney(Currency currency, String amount) {
+        if ((currency != null) && (amount != null)) {
+            BigDecimal amountValue;
+            try {
+                amountValue = new BigDecimal(amount);
+            } catch (Exception ex) {
+                amountValue = null;
+            }
+            if (amountValue != null) {
+                return new Money(currency, amountValue);
+            } else {
+                return null;
+            }
         } else {
             return null;
         }
