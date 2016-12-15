9d7011724d87e3bb7d4a29393b707431417188dc
Nikolay Nikolov <niksan9411@gmail.com>
2016-12-16 00:45:56 +0200

    add base construction on boundary clas for the database

--- src/networking/DatabaseController.java
+++ src/networking/DatabaseController.java
@@ -1,7 +1,38 @@
 package networking;
 
+import java.sql.*;
+import java.sql.Connection;
+
 /**
- * Created by Nikolay on 12/16/2016.
+ * Created by Nikolay on 12/14/2016.
  */
-public class DatabaseController {
+public abstract class DatabaseController {
+
+    public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://localhost/banksystem";
+
+    public static final String user = "root";
+    public static final String password = "";
+
+    private Connection connDatabase;
+
+    public DatabaseController(){
+        // connection to the database
+        try {
+            Class.forName(this.JDBC_DRIVER);
+            this.connDatabase = DriverManager.getConnection(this.DB_URL,this.user,this.password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();// or do log
+        } catch (SQLException e) {
+            e.printStackTrace();// or do log
+        }
+    }
+
+    public void close(){
+        try{
+            this.connDatabase.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
