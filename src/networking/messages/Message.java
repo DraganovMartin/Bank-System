6696aeb60adb5de5dd427d4db34b27f533641227
iliyan-kostov <iliyan.kostov.gml@gmail.com>
2016-12-16 08:14:23 +0200

    Updated "Message" and "MessageAuthenticator" field names according to database model. Added javadoc.

--- src/networking/messages/Message.java
+++ src/networking/messages/Message.java
@@ -11,34 +11,68 @@ import java.io.Serializable;
  * {@link Message#type} is implemented as {@link String} so that the class is
  * extensible and forward-compatible. It is expected that within the same system
  * all different derived classes use different values for the
- * {@link Message#type} field. The field {@link Message#clientPrimaryKeyValue}
- * can be used for setting or accessing either the sender or the receiver for
- * the message. The client ID for a specific {@link Message} is intended to be
- * set and controlled by the server through authentication procedures once the
- * message enters the system's boundary.
+ * {@link Message#type} field. The field {@link Message#clientID} can be used
+ * for setting or accessing either the sender or the receiver for the message.
+ * The client ID for a specific {@link Message} is intended to be set and
+ * controlled by the server through authentication procedures once the message
+ * enters the system's boundary. The value of the field corresponds to a client
+ * primary key in the database (Clients.ClientID).
  *
  * @author iliyan-kostov <iliyan.kostov.gml@gmail.com>
  */
 public abstract class Message implements Serializable {
 
-    private String clientPrimaryKeyValue;
+    /**
+     * The client ID. The field {@link Message#clientID} can be used for setting
+     * or accessing either the sender or the receiver for the message. The
+     * client ID for a specific {@link Message} is intended to be set and
+     * controlled by the server through authentication procedures once the
+     * message enters the system's boundary. The value of the field corresponds
+     * to a client primary key in the database (Clients.ClientID).
+     */
+    private String clientID;
 
+    /**
+     * The {@link Message} type. This field is used to recognize the specific
+     * derived class in order to be able to successfully typecast the base
+     * serialized {@link Message} reference to its actual intended type.
+     */
     private final String type;
 
+    /**
+     * Constructor.
+     *
+     * @param type the {@link Message} type.
+     */
     public Message(String type) {
         this.type = type;
-        this.clientPrimaryKeyValue = null;
+        this.clientID = null;
     }
 
+    /**
+     * Returns the {@link Message} type.
+     *
+     * @return the {@link Message} type.
+     */
     public final String getType() {
         return this.type;
     }
 
-    public final String getClientPrimaryKeyValue() {
-        return this.clientPrimaryKeyValue;
+    /**
+     * Returns the client ID.
+     *
+     * @return the client ID.
+     */
+    public final String getClientID() {
+        return this.clientID;
     }
 
-    public final void setClientPrimaryKeyValue(String clientPrimaryKeyValue) {
-        this.clientPrimaryKeyValue = clientPrimaryKeyValue;
+    /**
+     * Sets the client ID.
+     *
+     * @param clientID the client ID.
+     */
+    public final void setClientID(String clientID) {
+        this.clientID = clientID;
     }
 }
