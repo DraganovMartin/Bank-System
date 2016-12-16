6c8bc22bfca9ebed631df90ba25ae9d41d9a5203
iliyan-kostov <iliyan.kostov.gml@gmail.com>
2016-12-16 09:41:46 +0200

    Started a class for client-server test.

---
+++ src/testClasses/networking/client_server_test/ClientServerTest.java
@@ -0,0 +1,10 @@
+package testClasses.networking.client_server_test;
+
+/**
+ * A class for testing client-server.
+ *
+ * @author iliyan-kostov <iliyan.kostov.gml@gmail.com>
+ */
+public class ClientServerTest {
+
+}
