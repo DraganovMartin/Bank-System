7166fdd363b44b3d14114c6789534ddc550b4bd1
Nikolay Nikolov <niksan9411@gmail.com>
2016-12-19 22:15:00 +0200

    add test for get a client

--- src/testClasses/database/ClientTest.java
+++ src/testClasses/database/ClientTest.java
@@ -1,7 +1,26 @@
 package testClasses.database;
 
+import dataModel.models.Client;
+import database.DatabaseController;
+
+import java.sql.SQLException;
+
 /**
  * Created by Nikolay on 12/19/2016.
  */
 public class ClientTest {
+    public static void main(String[] args){
+        DatabaseController dc = new DatabaseController();
+        Client client = null;
+        try {
+            client = dc.getClient(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if(client.getFirstName().equals("Niki") && client.getLastName().equals("Miki")){
+            System.out.println(true);
+        }
+
+        dc.close();
+    }
 }
