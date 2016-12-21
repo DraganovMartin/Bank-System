3a8377c3aef546575449f4aed78a7f307b3e0093
Nikolay Nikolov <niksan9411@gmail.com>
2016-12-21 13:15:42 +0200

    create some tests

--- src/testClasses/database/registrate.java
+++ src/testClasses/database/registrate.java
@@ -1,7 +1,13 @@
 package testClasses.database;
 
+import database.DatabaseController;
+
 /**
  * Created by Nikolay on 12/21/2016.
  */
 public class registrate {
+    public static void main(String[] args){
+        DatabaseController db = new DatabaseController();
+         System.out.println(db.registrate("miksan","1234",1,1));
+    }
 }
--- src/testClasses/password_test/test1.java
+++ src/testClasses/password_test/test1.java
@@ -1,7 +1,25 @@
 package testClasses.password_test;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by Nikolay on 12/21/2016.
  */
 public class test1 {
+    public static void main(String []args){
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update("12345".getBytes());
+            byte byteData[] = md.digest();
+
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++)
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+
+            System.out.println("Digest(in hex format):: " + sb.toString());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
 }
